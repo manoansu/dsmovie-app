92d47d862c9e6192f3a3860eec3610b1c6cbd949
Ansumane Mané <manoansu@gmail.com>
2022-01-16 17:03:06 +0000

    useState and useEffet

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,30 @@
 import axios from 'axios';
 import MovieCard from 'Components/MovieCard';
+import { Movie, MoviePage } from 'pages/types/movie';
 import Pagination from 'Pagination';
+import { useEffect, useState } from 'react';
 import { BASE_URL } from 'utils/request';
 import './styles.css';
 
 const Listing = () =>{
 
+    // Mantem o estado do componente para variavel pageNumber, de acordo com as alterações
+    const [pageNumeber, setPageNumber] = useState(0);
+
     //FORMA ERRADA
-	 axios.get(`${BASE_URL}/movies?size=12&page=0`)
-     .then(response =>{
-         console.log(response.data);
-     });
+    useEffect(() =>{
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response =>{
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+            console.log(data);
+        });
+    }, []);
+	
 
     return(
         <>
+        <p>{pageNumeber}</p>
         <Pagination />
         <div className="container">
             <div className="row">
